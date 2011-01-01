d359f51ee7e099c8ad8ea3b62c5622365ede0f1f
bcoe <bencoe@gmail.com>
2011-01-01 15:20:11 -0500

    First commit of Mongate.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="mongate",
+    version="0.0.1",
+    description="Wrapper for Sleepy Mongoose that provides the same interface as Pymongo.",
+    author="Benjamin Coe",
+    author_email="bencoe@gmail.com",
+    url="https://github.com/bcoe/mongate",
+    packages = find_packages(),
+    install_requires = ['pymongo', 'httplib2'],
+    tests_require=['nose', 'coverage']
+)
